eda678dee64813e264ce034f44fd7f36b1a5d525
= <=>
2020-04-13 00:12:26 -0600

    Funcionabilidad de botones segun calculadora w10

--- app/src/main/java/ues/fmocc/calculadora/MainActivity.java
+++ app/src/main/java/ues/fmocc/calculadora/MainActivity.java
@@ -9,13 +9,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    public TextView txtResultado;
+    private TextView txtResultado;
+    private TextView txtHistorico;
+
+    //Datos para recordar seleccion
+    private String operator;
+    private int lastBnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtResultado = (TextView)findViewById(R.id.txtResultado);
+        txtHistorico = (TextView)findViewById(R.id.txtHistorico);
 
         for (int i = 0; i <= 9; i++) {
             int id = getResources().getIdentifier("btn"+i, "id", getPackageName());
@@ -31,6 +37,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ((Button) findViewById(R.id.btnCuadrado)).setOnClickListener(this);
         ((Button) findViewById(R.id.btnRaiz)).setOnClickListener(this);
         ((Button) findViewById(R.id.btnIgual)).setOnClickListener(this);
+        ((Button) findViewById(R.id.btnBorrar)).setOnClickListener(this);
+
+        operator = "";
+        lastBnt = 0;
 
     }
 
@@ -47,9 +57,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
+    public String removeLastCharacter(String str) {
+        String result = "";
+        if ((str != null) && (str.length() > 0)) {
+            result = str.substring(0, str.length() - 1);
+        }
+        return result;
+    }
+
+    //Metodo valida que no exista un valor con punto decimal a medias (. NO)-(5. NO)-(5.5 SI)
+    public boolean validInput(String cadena){
+        if(cadena.length() == 0){
+            return false;
+        }
+
+        if(cadena.indexOf('.') == cadena.length() - 1){
+            return false;
+        }
+        return true;
+    }
+
+    public String editOperator(String str, String newOpe){
+        if (str.isEmpty()){
+            return "";
+        }
+
+        String result = str;
+        if ((str != null) && (str.length() > 0)) {
+            result = str.substring(0, str.length() - 2);
+            result += result + newOpe + " ";
+        }
+        return result;
+    }
+
+    public boolean needsToClearInput(int lastButton){
+        int operatations[] = {R.id.btnSuma, R.id.btnResta, R.id.btnMultiplicacion, R.id.btnDivision};
+        for (int i = 0; i < operatations.length; i++) {
+            if(operatations[i] == lastButton){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isButtonNumber(int idBtn){
+        for (int i = 0; i <= 9; i++) {
+            int id = getResources().getIdentifier("btn"+i, "id", getPackageName());
+            if(id == idBtn){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        int currentBtn  = v.getId();
+
+        if(needsToClearInput(lastBnt) && isButtonNumber(currentBtn)){
+            txtResultado.setText("");
+        }
+
+        switch (currentBtn) {
             case R.id.btn0:
                 txtResultado.setText(txtResultado.getText() + "0");
                 break;
@@ -87,7 +156,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btnClear:
                 txtResultado.setText("");
+                txtHistorico.setText("");
+                break;
+            case R.id.btnBorrar:
+                txtResultado.setText(removeLastCharacter(txtResultado.getText().toString()));
+                break;
+            case R.id.btnSuma:
+                operator = "+";
+                if(lastBnt != R.id.btnSuma){
+
+                    if(validInput(txtResultado.getText().toString())){
+                        txtHistorico.setText(txtHistorico.getText().toString() + (txtHistorico.getText().length()==0?"":" + ") + txtResultado.getText());
+                    }
+
+                }
                 break;
         }
+        lastBnt = currentBtn;
     }
 }
